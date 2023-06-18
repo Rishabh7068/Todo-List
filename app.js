55030f7dd5351d32afa4b4b30602a4e8dc2bd840
Rishabh Rathore <r123ishabh@gmail.com>
2023-06-18 22:10:52 +0530

    template and layout

--- app.js
+++ app.js
@@ -54,6 +54,12 @@ app.post("/work", function (req, res) {
   res.redirect("/work");
 });
 
+app.get("/about",function(req,res){
+  res.render("about",{
+
+  });
+});
+
 app.listen(3000, function () {
   console.log("I am lIve at localPort 3000");
 });
